673c6115adddc104afb60051a2d4fd21821a2937
faf4r <81461670+faf4r@users.noreply.github.com>
2024-06-29 09:53:14 +0800

    Create category_download_async.py
    使用异步库异步下载各视频，下载更快

---
+++ category_download_async.py
@@ -0,0 +1,154 @@
+ import re
+import json
+import os
+import time
+import sys
+import asyncio
+
+import aiofiles
+import requests
+import httpx
+from loguru import logger
+
+logger.add(
+    sys.stdout,
+    level="INFO",
+    format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {module}:{function}:{line} - {message}",
+    # format="{message}",
+)
+
+reg = re.compile("<script>window.__playinfo__=(.*?)</script>")
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 Edg/124.0.0.0",
+    "Origin": "https://www.bilibili.com",
+    "Referer": "https://space.bilibili.com/",
+    "Cookie": "=============填写你自己的cookie===========",
+}
+
+mid = "1803865534"  # up的uid
+
+
+def merge(video_in, audio_in, out):
+    os.system(f'ffmpeg -i "{video_in}" -i "{audio_in}" -c copy -y "{out}"')
+
+
+def check(title):
+    mode = re.compile(r'[\\/:*?"<>|]')
+    new_name = re.sub(mode, "_", title)
+    return new_name
+
+global done_count   # 用于记录下载完成的个数
+
+async def download_unit(no, total, name, archive, client):
+    # logger.debug(no)
+    aid = archive["aid"]  # archive id
+    bvid = archive["bvid"]  # 视频号
+    title = archive["title"]  # 视频标题
+    title = check(title)
+    # logger.debug(str(no), title)
+    video_page_url = f"https://www.bilibili.com/video/{bvid}/"  # 结尾必须有/，不然cookie没用，出不了1080p
+    # print(f"\r进度: {no}/{total} {title} {video_page_url}", end="")
+    # logger.debug(video_page_url)
+
+    html = await client.get(url=video_page_url, headers=headers)
+    data = reg.findall(html.text)[0]
+    data = json.loads(data)
+    # logger.debug(data)
+
+    # with open('data.json', 'w', encoding='utf-8') as f:
+    #     json.dump(data, f)
+
+    video_url = data["data"]["dash"]["video"][0]["baseUrl"]
+    res = await client.get(video_url, headers=headers)
+    async with aiofiles.open(f"video/{name}/{no}_{title}.flv", "wb") as f:
+        await f.write(res.content)
+
+    audio_url = data["data"]["dash"]["audio"][0]["baseUrl"]
+    res = await client.get(audio_url, headers=headers)
+    async with aiofiles.open(f"audio/{name}/{no}_{title}.mp3", "wb") as f:
+        await f.write(res.content)
+
+    global done_count
+    done_count += 1
+    # merge(f'video/{name}/{no}_{title}.flv', f'audio/{name}/{no}_{title}.mp3', f'out/{name}/{no}_{title}.flv')
+    time.sleep(1)
+
+async def download_season(season_id, name, total, start_index=1):
+    page_size = 100  # 这里page_size不能太大，100是可以的
+    page_max_num = total // page_size + (total % page_size != 0)  # 向上取整
+
+    os.makedirs(f"video/{name}/", exist_ok=True)
+    os.makedirs(f"audio/{name}/", exist_ok=True)
+    os.makedirs(f"out/{name}/", exist_ok=True)
+
+    archive_list = []
+    for page_num in range(1, page_max_num + 1):
+        archive_list_url = f"https://api.bilibili.com/x/polymer/web-space/seasons_archives_list?mid={mid}&season_id={season_id}&sort_reverse=false&page_num={page_num}&page_size={page_size}"
+        # logger.debug(archive_list_url)
+
+        archive_res = requests.get(url=archive_list_url, headers=headers).json()
+        archive_list.extend(archive_res["data"]["archives"])
+    archive_list = [(index + 1, item) for index, item in enumerate(archive_list)]
+    archive_list = archive_list[start_index - 1 :]
+    # logger.debug(len(archive_list))
+
+    global done_count
+    done_count = 0
+    async with httpx.AsyncClient(timeout=None) as client:   # timeout需为None，否则很容易readTimeout
+        print("下载中...")
+        task_list = []
+        # 太快了B站会关闭连接，所以考虑分片，每次8个一起下
+        for i, (no, archive) in enumerate(archive_list):
+            task_list.append(download_unit(no, total, name, archive, client))
+            if i % 8 == 0:
+                await asyncio.gather(*task_list)
+                task_list = []
+                time.sleep(5)   # 限一下速不然会挂
+        if i % 8 != 0:  # 剩余没执行部分
+            await asyncio.gather(*task_list)
+        # while done_count < total:
+        #     print(f"\r下载进度: {done_count}/{total}", end="\n")
+        #     await asyncio.sleep(1)
+        print("done.")
+
+async def main():
+    series_page_num = 1
+    series_list_url = f"https://api.bilibili.com/x/polymer/web-space/seasons_series_list?mid={mid}&page_num={series_page_num}&page_size=20"  # 这里的page_size最大好像就这个
+
+    seasons_list_json = requests.get(url=series_list_url, headers=headers).json()
+    seasons_list = seasons_list_json["data"]["items_lists"]["seasons_list"]
+
+    # # 下载全部合集
+    # for season in seasons_list:
+    #     name = season['meta']['name']           # 合集名
+    #     total = season['meta']['total']         # 合集总视频数
+    #     season_id = season['meta']['season_id'] # 合集id
+    #     print(name)
+
+    #     download_season(season_id, name, total, 1)
+    #     for file in os.listdir(f'./video/{name}'):
+    #         file = file[:-4]
+    #         merge(f'./video/{name}/{file}.flv', f'./audio/{name}/{file}.mp3', f'./out/{name}/{file}.flv')
+
+    # 下载指定合集
+    for season in seasons_list:
+        name = season["meta"]["name"]  # 合集名
+        total = season["meta"]["total"]  # 合集总视频数
+        season_id = season["meta"]["season_id"]  # 合集id
+        if season_id not in [2130269]:  # 注意是int型
+            continue
+        print(name)
+
+        await download_season(season_id, name, total, 1)
+
+        for file in os.listdir(f"./video/{name}"):
+            file = file[:-4]
+            merge(
+                f"./video/{name}/{file}.flv",
+                f"./audio/{name}/{file}.mp3",
+                f"./out/{name}/{file}.flv",
+            )
+
+if __name__ == "__main__":
+    asyncio.run(main())
